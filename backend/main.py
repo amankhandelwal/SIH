84d4e0a60b6735bff476ee9e8c9b06c3e11ede0e
Anirban Ghosh <anirbang14@gmail.com>
2018-03-12 22:54:15 +0530

    templates

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 ## Important imports ##
 from flask import Flask, render_template
-
+import MySQLdb
+import json
 ##neccesary code##
 app = Flask(__name__)
 
@@ -8,7 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('homepage.html')
- 
+@app.route("/login")
+def login():
+	#db = MySQLdb.connect("localhost","root","",)
+    return render_template('login.html') 
 ## To run the Flask##
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=5000,debug=True)
