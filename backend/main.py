d4fca62f5dacba708dcbbd4e5e560e6e619dc926
Rohit Agarwal <madudeyo@gmail.com>
2018-03-12 21:54:03 +0530

    login done

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 ## Important imports ##
-from flask import Flask, render_template
+import MySQLdb
+from flask import Flask, request, Response ,jsonify
+import json
 
 ##neccesary code##
 app = Flask(__name__)
@@ -9,6 +11,29 @@ app = Flask(__name__)
 def hello():
     return render_template('homepage.html')
  
+@app.route("/login", methods = ['POST'])
+def login():
+	#res = request.get_json()
+	#get the username
+	password = request.form['password']
+	policeId = request.form['policeId']
+	
+	db = MySQLdb.connect("localhost","root","","hackathon")
+	cursor = db.cursor()
+
+	cursor.execute("SELECT * from policedetails where policeId='"+policeId+"' and password='"+password+"'")
+	pwd = cursor.fetchone()
+
+	if (pwd):
+
+		db.close()
+		return "Success"
+		#return jsonify({"status":"Success"})
+	else:
+		db.close()
+		return "Failed"
+		#return jsonify({"status":"Failed"})
+
 ## To run the Flask##
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5001, debug=True)
